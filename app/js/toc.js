dcfb13c50eb4a67ae6297fcae454406a1205d204
Pev <pev.arfan@gmail.com>
2015-09-09 09:57:45 +0300

    Add GeoJson format

--- app/js/toc.js
+++ app/js/toc.js
@@ -4,7 +4,29 @@ $( document ).ready(function() {
 	// Initialise toc values
 	var message = "";
 
-	// TODO : Get JSON
+	// Get JSON from URL
+    $.ajax({ 
+        type: "GET", 
+        url: "./js/toc.json", 
+        //url: "http://api.geonames.org/citiesJSON?",
+        dataType:'json',
+        success: function(response){
+
+            alert("Success");
+            
+            // Parse JSON
+            message = ""
+            for(key in response.geometry){
+                alert(key);
+            }           
+
+        },  
+        error: function(){
+            alert("fail load json");
+        },
+    }); 
+
+
 	var getJson = true;
 
 	// If we have a success
@@ -13,6 +35,17 @@ $( document ).ready(function() {
 		// TODO : Parse JSON to format TOC
 		// TODO : Create function : check the box, refresh the map
 		message += "<ul data-role=\"listview\">"
+
+        + "<div data-role=\"collapsible\">"
+        + "  <h3>Tiles</h3>"
+        + "  <fieldset data-role=\"controlgroup\">"
+        + "    <input name=\"radio-choice-osm\" id=\"radio-choice-osm\" value=\"choice-osm\" checked=\"checked\" type=\"radio\">"
+        + "    <label for=\"radio-choice-osm\">OSM</label>"
+        + "    <input name=\"radio-choice-sat\" id=\"radio-choice-sat\" value=\"choice-sat\" type=\"radio\">"
+        + "    <label for=\"radio-choice-sat\">SAT</label>"
+        + "  </fieldset>"
+        + "</div>"
+
         + "<div data-role=\"collapsible\">"
         + "  <h3>Section 1</h3>"
         + "  <fieldset data-role=\"controlgroup\">"
@@ -26,6 +59,7 @@ $( document ).ready(function() {
         + "    <label for=\"checkbox-4a\"><small>Sun Chips</small></label>"
         + "  </fieldset>"
         + "</div>"
+
         + "<div data-role=\"collapsible\">"
         + "  <h3>Section 2</h3>"
         + "  <fieldset data-role=\"controlgroup\">"
@@ -33,10 +67,12 @@ $( document ).ready(function() {
         + "    <label for=\"checkbox-5a\"><small>Other</small></label>"
         + "  </fieldset>"
         + "</div>"
+
         + "<div data-role=\"collapsible\">"
         + "  <h3>Section 3</h3>"
         + "  <p>I'm the collapsible content for section 3</p>"
         + "</div>"
+
     + "</ul>";
 
 	};

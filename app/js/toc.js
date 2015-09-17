74b0d5eb80ad0bbf0397af0e1b4aa2b90bea5bea
Pev <pev.arfan@gmail.com>
2015-09-17 14:06:53 +0300

    Back to hard file if TOC not works

--- app/js/toc.js
+++ app/js/toc.js
@@ -7,6 +7,8 @@
 // When the document is ready, we load the TOC
 $( document ).ready(function() {
 
+    alert("Hello");
+
     // Initialise toc values
     var message = "";
 
@@ -71,8 +73,47 @@ $( document ).ready(function() {
             } else {
                 alert('Uncaught Error.\n' + jqXHR.responseText);
             }
+
+            var message = "<h3>Hard Code</h3><ul data-role=\"listview\">"
+                    + "<div data-role=\"collapsible\">"
+                    + "  <h3>Tiles</h3>"
+                    + "  <fieldset data-role=\"controlgroup\">"
+                    + "    <input name=\"radio-choice-osm\" id=\"radio-choice-osm\" value=\"choice-osm\" checked=\"checked\" type=\"radio\">"
+                    + "    <label for=\"radio-choice-osm\">OSM</label>"
+                    + "    <input name=\"radio-choice-sat\" id=\"radio-choice-sat\" value=\"choice-sat\" type=\"radio\">"
+                    + "    <label for=\"radio-choice-sat\">SAT</label>"
+                    + "  </fieldset>"
+                    + "</div>"
+                    + "<div data-role=\"collapsible\">"
+                    + "  <h3>Section 1</h3>"
+                    + "  <fieldset data-role=\"controlgroup\">"
+                    + "    <input name=\"checkbox-1a\" id=\"checkbox-1a\" checked=\"\" type=\"checkbox\">"
+                    + "    <label for=\"checkbox-1a\"><small>Cheetos</small></label>"
+                    + "    <input name=\"checkbox-2a\" id=\"checkbox-2a\" type=\"checkbox\">"
+                    + "    <label for=\"checkbox-2a\"><small>Doritos</small></label>"
+                    + "    <input name=\"checkbox-3a\" id=\"checkbox-3a\" type=\"checkbox\">"
+                    + "    <label for=\"checkbox-3a\"><small>Fritos</small></label>"
+                    + "    <input name=\"checkbox-4a\" id=\"checkbox-4a\" type=\"checkbox\">"
+                    + "    <label for=\"checkbox-4a\"><small>Sun Chips</small></label>"
+                    + "  </fieldset>"
+                    + "</div>"
+                    + "<div data-role=\"collapsible\">"
+                    + "  <h3>Section 2</h3>"
+                    + "  <fieldset data-role=\"controlgroup\">"
+                    + "    <input name=\"checkbox-5a\" id=\"checkbox-5a\" checked=\"\" type=\"checkbox\">"
+                    + "    <label for=\"checkbox-5a\"><small>Other</small></label>"
+                    + "  </fieldset>"
+                    + "</div>"
+                    + "<div data-role=\"collapsible\">"
+                    + "  <h3>Section 3</h3>"
+                    + "  <p>I'm the collapsible content for section 3</p>"
+                    + "</div>"
+                + "</ul>";
+
+            // Write TOC content
+            $("#app_toc").html(message).trigger("create");
         }
-        
+
     });
 
 });

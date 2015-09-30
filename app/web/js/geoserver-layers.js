b13de670e40495448d16bc9f5af723b213498948
František Kolovský <kolovsky@thinkpad.kolovsky>
2015-09-30 10:35:26 +0200

    add geoserver layers

--- app/web/js/geoserver-layers.js
+++ app/web/js/geoserver-layers.js
@@ -8,3 +8,30 @@
 
 // TODO : COMMENT !!
 
+
+//retrurn list of layers
+function getGeoserverLayers(url){
+	var geojson_data;
+
+	$.ajax({
+	    type: "GET",
+	    url: "example.json",
+	    contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+		success: function(data){
+			geojson_data = data;
+	    },
+	    error: function(jqXHR, exception) {
+	        alert('Load problem');    
+	    }
+
+	    });
+
+	var myLayer = L.geoJson();
+
+	myLayer.addData(geojson_data);
+
+	return [myLayer]
+
+};
+

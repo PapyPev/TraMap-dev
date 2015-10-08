fc3d7ca77b4c0f28f9c56b413a1e3f06f9cfe77c
Pev <pev.arfan@gmail.com>
2015-10-08 11:32:44 +0300

    Test getLayerList from getCapabilities

--- app/web/js/actionGeoServerLayers.js
+++ app/web/js/actionGeoServerLayers.js
@@ -51,62 +51,30 @@ function getGeoServerLayers(url, user, password, repository){
   // Return value : list of layers
   var listOfLayers = [];
 
-  $.ajax({
+  if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+  }
+  else
+  {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
 
-    // POST Parameters
-    type: 'POST',
-    url: url+'/'+repository+'/ows?SERVICE=WFS&REQUEST=GetCapabilities&user=\''
-      +user+'\'&password=\''+password+'\'',
-    contentType: 'text/xml; charset=utf-8',
-    dataType: 'xml',
+  xmlhttp.onload = function() {
+    var xmlDoc = new DOMParser().parseFromString(xmlhttp.responseText,'text/xml');
 
-    success: function (data) {
+    console.log(xmlDoc);
 
-      console.log(data);
-
-      if (window.XMLHttpRequest)
-      {// code for IE7+, Firefox, Chrome, Opera, Safari
-          xmlhttp=new XMLHttpRequest();
-      }
-      else
-      {// code for IE6, IE5
-          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-
-      xmlhttp.onload = function() {
-        var xmlDoc = new DOMParser().parseFromString(xmlhttp.responseText,'text/xml');
-
-        console.log(xmlDoc);
-
-        document.write("<table border='1'>");
-        var x=xmlDoc.getElementsByTagName("CD");
-        for (i=0;i<x.length;i++)
-        { 
-            document.write("<tr><td>");
-            document.write(x[i].getElementsByTagName("c_id")[0].childNodes[0].nodeValue);
-            document.write("</td><td>");
-            document.write(x[i].getElementsByTagName("facebook_id")[0].childNodes[0].nodeValue);
-            document.write("</td></tr>");
-        }
-        document.write("</table>");
-
+    var x=xmlDoc.getElementsByTagName("FeatureTypeList");
+    for (i=0;i<x.length;i++)
+    { 
+        console.log(document.write(x[i].getElementsByTagName("FeatyreType")[0].childNodes[0].nodeValue));
     }
+  }
 
 
-    xmlhttp.open("GET",url+'/'+repository+'/ows?SERVICE=WFS&REQUEST=GetCapabilities',false);
-    xmlhttp.send();
-
-    },
-
-    error: function(jqXHR, exception){
-      if (jqXHR.status === 401) {
-        console.log('HTTP Error 401 Unauthorized.');
-      } else {
-        console.log('Uncaught Error.\n' + jqXHR.responseText);
-      }
-    }
-
-  });
+  xmlhttp.open("GET",url+'/'+repository+'/ows?SERVICE=WFS&REQUEST=GetCapabilities',true, user, password);
+  xmlhttp.send();
 
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~ NOK ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

32462429c9f1077035442da68302f1f8363c44cd
Pev <pev.arfan@gmail.com>
2015-10-20 11:49:59 +0300

    test fix bbox GeoServer query

--- app/web/js/actionGeoServerLayers2.js
+++ app/web/js/actionGeoServerLayers2.js
@@ -82,8 +82,9 @@ function getGeoServerLayers(url, repository, projection, bbox){
           +"/ows?service=WFS&version=1.0.0&request=GetFeature&typeName="
           +repository+":"+layerName
           +"&srsName="+projection
-          +"&bbox="+bbox._northEast.lat+","+bbox._northEast.lng+","
-          +bbox._southWest.lat+","+bbox._southWest.lng
+          +"&SRS=epsg:4326"
+          +"&bbox="+bbox._southWest.lat+","+bbox._southWest.lng+","
+          +bbox._northEast.lat+","+bbox._northEast.lng
           +"&maxFeatures=100&outputFormat=application/json"//,
           // {
           //   style: setStyle

5ee6a844b4a84acb6a3729a7f31244d74aceeb1f
Pev <pev.arfan@gmail.com>
2015-10-22 15:56:12 +0300

    test Prepare Click - Cursor and ESC

--- app/web/js/actionPopupEvents.js
+++ app/web/js/actionPopupEvents.js
@@ -12,11 +12,9 @@
 // Number of clicks for buttonSearch
 var nbClick;
 
-
 /* ============================================================================
  * FUNCTIONS
  * ========================================================================= */
-
 
 /**
  * Get form value from focus popup and focus on values
@@ -39,12 +37,28 @@ function buttonSearch () {
   // Change cursor symbol
   $('.leaflet-container').css('cursor','crosshair');
 
+  // Active click on the map
   map.on('click', function(e) {
     alert(e.containerPoint.toString() + ', ' + e.latlng.toString());
   });
 
   // Remove cursor style
   //$('.leaflet-container').css('cursor','');
+
+  // If esc is pressed
+  $(document).keyup(function(e) {
+     if (e.keyCode == 27) {
+
+        alert('ESC');
+
+        // Remove click event
+        map.off('click');
+
+        // Remove cursor style
+        $('.leaflet-container').css('cursor','');
+    }
+
+  });
 
 }; //--- end buttonSearch()
 

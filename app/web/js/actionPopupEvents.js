62304aee00ac023a8735adee926f0c00411ac814
Pev <pev.arfan@gmail.com>
2015-10-22 15:20:53 +0300

    test Prepare Focus

--- app/web/js/actionPopupEvents.js
+++ app/web/js/actionPopupEvents.js
@@ -7,9 +7,9 @@
 
 function buttonFocus () {
 
-  var key = document.getElementById("focusKeyword");
-  var lat = document.getElementById("focusLatLong-Lat");
-  var lon = document.getElementById("focusLatLong-Long");
+  var key = document.getElementById("textFocusKeyword");
+  var lat = document.getElementById("textFocusLatLong-Lat");
+  var lon = document.getElementById("textFocusLatLong-Long");
 
   alert(key.innerHTML, lat.innerHTML, lon.innerHTML);
 

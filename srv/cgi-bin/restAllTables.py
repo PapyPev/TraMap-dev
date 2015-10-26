4949ae4f98f1a11116bece4baf1f4bdda30002e4
Pev <pev.arfan@gmail.com>
2015-10-26 14:16:28 +0200

    test: restAllTables

--- srv/cgi-bin/restAllTables.py
+++ srv/cgi-bin/restAllTables.py
@@ -22,28 +22,32 @@ import classDatabase
 # =============================================================================
 
 def get_allTables():
-    """
-        Return all table's names from database
-    """
+  """
+      Return all table's names from database
+  """
 
-    # Create default database connexion object
-    db = classDatabase.Database()
+  # Create default database connexion object
+  db = classDatabase.Database()
 
-    # Connexion to the database
-    db._connect()
+  # Connexion to the database
+  db._connect()
 
-    # Prepare the SQL query
-    sql = "SELECT table_name " \
-        "FROM information_schema.tables " \
-        "WHERE table_schema='public'"
+  # Prepare the SQL query
+  sql = "SELECT table_name " \
+      "FROM information_schema.tables " \
+      "WHERE table_schema='public'"
 
-    # Execute the query
-    rows = db._execute(sql)
-    
-    for row in rows:
-      print(row[0])
+  # Execute the query
+  rows = db._execute(sql)
 
-    #print res
+  # List of tables names
+  names = []
+  
+  # Loops results to get names
+  for row in rows:
+    names.append(row[0])
+
+  print names
     
 
 # MAIN

80eaa627a938b8180782378f3f692c18ab00bf9f
Pev <pev.arfan@gmail.com>
2015-10-26 09:45:50 +0200

    cgi: classDatabase.py

---
+++ srv/cgi/classDatabase.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+  classDatabase.py
+  This class contains all database informations and methods
+"""
+
+__author__ = "Pev"
+__version__ = "1.0"
+__email__ = "pev@gmx.fr"
+__status__ = "Progress"
+
+
+# IMPORT
+# =============================================================================
+import sys
+import os
+import psycopg2
+
+# MAIN CLASS
+# =============================================================================
+
+class Database(object):
+  """Class for Database configuration"""
+
+  # CONSTRUCTOR
+  # ---------------------------------------------------------------------------
+  def __init__(self, host, dbName, user, password):
+    """
+      Database constructor.
+
+      :Parameters:
+        host
+          The database host name
+        database
+          The database name
+        user
+          The user name
+        password
+          The user password
+
+      :Example:
+      >>> 
+
+    """
+    self.host = host
+    self.dbName = dbName
+    self.user = user
+    self.password = password
+
+    self.connect = None
+    self.cursor = None
+    
+    print('classDatabase.__init__(' \
+      + self.host + self.dbName + self.user + '*****)')
+
+
+  # GETTER
+  # ---------------------------------------------------------------------------
+
+  def get_host():
+    """ Return the database's host. """
+    return self.host
+
+  def get_dbname():
+    """ Return the database's name. """
+    return self.dbName
+
+  def get_user():
+    """ Return the database user's name. """
+    return self.user
+
+
+  # SETTER
+  # ---------------------------------------------------------------------------
+
+  def set_host(host):
+    """
+      Change the host name.
+
+      :Parameters:
+        host
+          The new host name
+    """
+    self.host = host
+    print('classDatabase.setHost() -> ' + self.host)
+
+  def set_dbname(dbName):
+    """
+      Change the database name.
+
+      :Parameters:
+        database
+          The new database name (string)
+    """
+    self.dbName = dbName
+    print('classDatabase.setDbName() -> ' + self.dbName)
+
+  def set_user(user):
+    """
+      Change the user name.
+
+      :Parameters:
+        user
+          The new user name (string)
+    """
+    self.user = user
+    print('classDatabase.setUser() -> ' + self.user)
+
+  def set_password(password):
+    """
+      Change the password.
+
+      :Parameters:
+        password 
+          The new password (string)
+    """
+    self.password = password
+    print('classDatabase.setPassword() -> *****')
+
+
+  # METHODS
+  # ---------------------------------------------------------------------------
+
+  def _connect():
+    """ 
+      Create the database connection
+    """
+    try:
+
+      # Connection to the database
+      self.connect = psycopg2.connect( \
+        host = self.host, \
+        database = self.dbName, \
+        user = self.user, \
+        password = self.password)
+
+      # Init the database cursor
+      self.cursor = self.connect.cursor()
+
+      # Print the result
+      print('classDatabase._connect(): Connected.')
+
+    except Exception, e:
+      print('Error classDatabase._connect(): ' + e)
+
+  def _execute(sqlQuery):
+    """ 
+      Return the result of a SQL query.
+
+      :Parameters:
+        sqlQuery
+          The SQL query (string)
+    """
+    try:
+
+      # Execute the query
+      self.cursor.execute(sqlQuery)
+
+      # Save the result on list
+      rows = self.cursor.fetchall()
+
+      # Return the result
+      return rows
+
+    except Exception, e:
+      print('Error classDatabase._execute(): ' + e)
+      return None
+    
+
+

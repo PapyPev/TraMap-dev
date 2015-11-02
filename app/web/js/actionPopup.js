dbdda1bc9f295fad909189868ef390a49ccf3123
Pev <pev.arfan@gmail.com>
2015-11-02 12:53:33 +0200

    popupPointer: redmarker

--- app/web/js/actionPopup.js
+++ app/web/js/actionPopup.js
@@ -280,9 +280,7 @@ function buttonSearchByPointer () {
 
   // Init list of marker origin-destination (OD)
   if (listOD.length != 0) {
-    for (var i = 0; i < listOD.length; i++) {
-      map.removeLayer(listOD[i]);
-    };
+    alert("diff")
   };
   listOD = [];
 

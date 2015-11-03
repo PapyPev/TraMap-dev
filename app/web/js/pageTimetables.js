912dafd2d914ec9a68ffccd7dc5ce8ee5e923f03
Pev <pev.arfan@gmail.com>
2015-11-03 12:21:58 +0200

    timetable: Add Stations and Mini website

--- app/web/js/pageTimetables.js
+++ app/web/js/pageTimetables.js
@@ -3,22 +3,53 @@
  * Initialize Map content
  *
  * @author Pev
- * @version 1.0
+ * @version 1.1
  *************************************************************************** */
 
 /* ============================================================================
  * CONSTANTS
  * ========================================================================= */
 
-var DIGITRAFFIC = 'http://rata.digitraffic.fi/api/v1/live-trains?station='
+var DIGI_TRAFFIC = 'http://rata.digitraffic.fi/api/v1/live-trains?station='
+
+/* ============================================================================
+ * FUNCTIONS
+ * ========================================================================= */
+
+/**
+ * Load HTML content for Stations informations
+ * @param {json} data JSON response from digitraffic
+ --------------------------------------------------------------------------- */
+function loadTimetables (data) {
+  console.log(data);
+
+  //Init html
+  var htmlContent = '';
+
+  // Loop Trains
+  for (var i = 0; i < data.length; i++) {
+    htmlContent += '<h4>'+data[i].trainCategory + ': ' 
+    htmlContent += data[i].trainNumber
+    htmlContent += ' (' + data[i].trainType +')</h4>'
+    htmlContent += '<table class="table table-striped">'
+    htmlContent += ''
+    
+    // Loop time table
+    for (var j = 0; j < data[i].timeTableRows.length; j++) {
+      
+      htmlContent += '<tr><td>' + data[i].timeTableRows[j] + '</td></tr>'
+
+    };
+
+    htmlContent += '</table>'
+  };
+
+  $("#divResults").html(htmlContent);
+}
 
 /* ============================================================================
  * MAIN
  * ========================================================================= */
-
-function loadingTrainInformation (jsonInfo) {
-  console.log(jsonInfo);
-}
 
 /**
  * Action performed when the page is fully loaded
@@ -35,19 +66,17 @@ $(document).ready(function($) {
     var xhr = new XMLHttpRequest();
 
     // GET query
-    xhr.open('GET', DIGITRAFFIC+trainValue);
+    xhr.open('GET', DIGI_TRAFFIC+trainValue);
 
     // Load Content
     xhr.onreadystatechange = function () {
       if (this.status == 200 && this.readyState == 4) {
         //console.log('response: ' + this.responseText);
-        loadingTrainInformation(JSON.parse(this.responseText));
+        loadTimetables(JSON.parse(this.responseText));
       }
     };
 
     // Sent request
     xhr.send();
-
   }); //-- end $("#trainButton").click()
-
 }); //--- end $(document).ready()

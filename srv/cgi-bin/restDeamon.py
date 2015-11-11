5352de4e929352ba7fa4edca1063f268562296d4
PapyPev <pev.arfan@gmail.com>
2015-11-11 14:38:33 +0200

    rest: Shortest path test

--- srv/cgi-bin/restDeamon.py
+++ srv/cgi-bin/restDeamon.py
@@ -71,7 +71,7 @@ def api(service='default', lat1=0, lon1=0, lat2=0, lon2=0, param='default'):
     'default' : rest_default(),
     'simpleText' : rest_simpleText(),
     'interests' : rest_interests(),
-    'shortestPath': rest_shortestPath(request.args.get('lat1', ''), request.args.get('laon1', ''), lat2, lon2)
+    'shortestPath': rest_shortestPath(app.request.args.get('lat1', ''), app.request.args.get('laon1', ''), lat2, lon2)
   }.get(service, rest_default())
 
   # Return message

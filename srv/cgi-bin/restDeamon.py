8cd7295e394060bb3a6b67d9761b0ff5a048736d
PapyPev <pev.arfan@gmail.com>
2015-11-11 14:33:03 +0200

    rest: Shortest path test

--- srv/cgi-bin/restDeamon.py
+++ srv/cgi-bin/restDeamon.py
@@ -306,6 +306,7 @@ def rest_shortestPath(lat1, lon1, lat2, lon2):
 # =============================================================================
 
 if __name__ == "__main__":
+  app.debug = True
   app.run(port=8082)
 
 

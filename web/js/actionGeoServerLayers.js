f9645b9f96a444358fe65492622438a073b4a0dd
PapyPev <pev.arfan@gmail.com>
2015-11-16 15:07:01 +0200

    test: Layer Style

--- web/js/actionGeoServerLayers.js
+++ web/js/actionGeoServerLayers.js
@@ -51,7 +51,7 @@ function gs_setStyle(feature) {
         iconAnchor:   [17, 35], // point of the icon which will correspond to marker's location
         popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
       });
-      feature.setIcon(redMarker);
+      //feature.setIcon(redMarker);
       console.log(redMarker);
       console.log(feature);
       return {icon: redMarker};

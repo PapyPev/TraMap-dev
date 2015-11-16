b90d5307a50a8d4448e1efe241806fee2677a39b
PapyPev <pev.arfan@gmail.com>
2015-11-16 12:56:13 +0200

    test: Layer Style

--- web/js/actionGeoServerLayers.js
+++ web/js/actionGeoServerLayers.js
@@ -44,13 +44,14 @@ function gs_setStyle(feature) {
     //---------- Points Style
     case "Point":
     case "MultiPoint":
+      console.log("point");
 
       var redMarker = L.icon({
-    iconUrl: '../img/icon-map/marker.png',
-    iconSize:     [35, 35], // size of the icon
-    iconAnchor:   [17, 35], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
-  });
+        iconUrl: 'img/icon-map/marker.png',
+        iconSize:     [35, 35], // size of the icon
+        iconAnchor:   [17, 35], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
+      });
 
       console.log("point", icon, feature.properties.type);
       return {icon: redMarker};

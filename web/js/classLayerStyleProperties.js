57147e54b0ef3571c2897fe31db4d44c59853bdc
PapyPev <pev.arfan@gmail.com>
2015-11-19 13:32:31 +0200

    test: classLayerStyleProperties

--- web/js/classLayerStyleProperties.js
+++ web/js/classLayerStyleProperties.js
@@ -21,7 +21,7 @@
  * @this {LayerStyleProperties}
  * @param {String} filePath      [The layer style configuration file]
  */
-function LayerStyleProperties(filePath, name) {
+function LayerStyleProperties(filePath) {
 
   // Read configuration file from JSON
   var fromJson = getLayerStyleConfig(filePath);
@@ -35,57 +35,6 @@ function LayerStyleProperties(filePath, name) {
 
 } //-- end LayerStyleProperties(...)
 
-/**
- * [Creates an instance of LayerStyleProperties]
- * @constructor
- * @this {LayerStyleProperties}
- * @param {String} filePath      [The layer style configuration file]
- * @param {String} name      [The layer's name]
- */
-function LayerStyleProperties(filePath, name) {
-
-  // Read configuration file from JSON
-  var fromJson = getLayerStyleConfig(filePath, name);
-
-  /**
-   * [Name of the layer]
-   * @type {String}
-   * @private
-   */
-  this.name = fromJson.name
-
-  /**
-   * [Type of the layer (point, line, polygon)]
-   * @type {String}
-   * @private
-   */
-  this.type = fromJson.type
-
-  /**
-   * [If the layer have a specific style filters, If not : just one style]
-   * @type {Boolean}
-   * @private
-   */
-  this.filters = fromJson.filters
-
-  /**
-   * [If the layer have a specifi style, what kind of filer (bounds, number,
-   * words)]
-   * @type {String}
-   * @private
-   */
-  this.filters_type = fromJson.filters_type
-
-  /**
-   * [List of styles for the layer. The Object depend of type of layer. For
-   * all type of layer, style have "zoom_min" and "zoom_max" attributes.]
-   * @type {array}
-   * @private
-   */
-  this.styles = fromJson.styles
-
-} //-- end LayerStyleProperties(...)
-
 // ============================================================================
 // GETTERS
 // ============================================================================
@@ -94,50 +43,24 @@ function LayerStyleProperties(filePath, name) {
  * [Return the list of layers properties]
  * @return {String} [Layer's name]
  */
-LayerStyleProperties.prototype.getLayersStyle = function(){
+LayerStyleProperties.prototype.getLayers = function(){
   return this.layers;
 }
 
-/**
- * [Return the name of the layer]
- * @return {String} [Layer's name]
- */
-LayerStyleProperties.prototype.getName = function(){
-  return this.name;
-}
+LayerStyleProperties.prototype.getLayerStayle = function(name) {
+  
+  var style;
 
-/**
- * [Return the type of the layer (Point, Polygon, Line)]
- * @return {String} [Layer's type]
- */
-LayerStyleProperties.prototype.getType = function(){
-  return this.type;
-}
+  for (var i = this.layers.length - 1; i >= 0; i--) {
+    if (this.layers[i].name === name) {
+      style = this.layers[i];
+      break;
+    }
+  }
 
-/**
- * [Return if the layer have filters]
- * @return {Boolean} [If the layer have specifics filters it's true]
- */
-LayerStyleProperties.prototype.getFilters = function(){
-  return this.filters;
-}
+  return style;
 
-/**
- * [If the layer have specifics filters, return the filters type]
- * @return {String} [Filter's type (bounds, number, word)]
- */
-LayerStyleProperties.prototype.getFiltersType = function(){
-  return this.filters_type;
-}
-
-/**
- * [Return the Style object]
- * @return {json} [Object Style properties]
- */
-LayerStyleProperties.prototype.getStyles = function(){
-  return this.styles;
-}
-
+};
 
 // ============================================================================
 // METHODS
@@ -152,11 +75,7 @@ LayerStyleProperties.prototype.getStyles = function(){
  */
 LayerStyleProperties.prototype.toString = function() {
   var attributesToLog = [{
-    name : this.name,
-    type : this.type,
-    filters : this.filters,
-    filters_type : this.filters_type,
-    styles : this.styles
+    layers : this.layers
   }];
   return JSON.stringify(attributesToLog);
 };
@@ -171,7 +90,7 @@ LayerStyleProperties.prototype.toString = function() {
  * @param  {String} filePath [Path to the json file (or url)]
  * @return {json}          [Content configuration : JSON content]
  */
-function getLayerStyleConfig (filePath, name) {
+function getLayerStyleConfig (filePath) {
 
   // Returned value
   var layerStyleConfig;
@@ -202,48 +121,3 @@ function getLayerStyleConfig (filePath, name) {
   return layerStyleConfig;
 } //-- end getContentConfig (filePath)
 
-/**
- * [Get GeoServer Configurations from JSON file]
- * @this {LayerStyleProperties}
- * @param  {String} filePath [Path to the json file (or url)]
- * @return {json}          [Content configuration : JSON content]
- */
-function getLayerStyleConfig (filePath, name) {
-
-  // Returned value
-  var layerStyleConfig;
-
-  // Get JSON
-  $.ajax({
-    type: 'GET',
-    url: filePath,
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
-    success: function(data){
-
-      console.warn("data")
-
-      for (var i = data.layers.length - 1; i >= 0; i--) {
-        if (data.layers[i].name === name) {
-          layerStyleConfig = data.layers[i];
-          break;
-        }
-      }
-
-      console.log(layerStyleConfig);
-
-    },
-    error: function(jqXHR, exception){
-      console.warn("failde")
-      if (jqXHR.status === 401) {
-        console.log('HTTP Error 401 Unauthorized.');
-      } else {
-        console.log('Uncaught Error.\n' + jqXHR.responseText);
-      }
-    },
-    async: false
-
-  });
-
-  return layerStyleConfig;
-} //-- end getContentConfig (filePath)

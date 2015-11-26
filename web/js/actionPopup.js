cd749a63ea9db4a06d76ed77277d928c5b0c2804
PapyPev <pev.arfan@gmail.com>
2015-11-26 13:30:51 +0200

    Update leaflet version

--- web/js/actionPopup.js
+++ web/js/actionPopup.js
@@ -31,7 +31,7 @@ function popup_getIntinerary(origin, destination) {
 
   console.log('popup_getIntinerary');
 
-  console.log(origin)
+  console.log(origin.getLat())
 
   $.ajax({
     type: 'GET',
